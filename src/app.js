2f42c0485c921794c2ef6c044b887329c0d009f0
Navez <navezsheikh0731@gmail.com>
2021-07-01 17:25:25 +0530

    setup app for heroku correction in src/app.js, js/app.js, package.json

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ const path = require('path')
 const hbs = require('hbs')
 //Creation of Express application
 const express = require('express')
+
 const app = express()
+const port = process.env.PORT || 3000
+
+
 const geocode = require('./utils/geocoding')
 const forecast = require('./utils/forecast')
 
@@ -99,6 +103,6 @@ app.get('*', (req, res) => {
    })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port 3000.' + port)
 })
